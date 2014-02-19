6f971473da12c1884d5e7396bb062001882a82ca
Nicolas Hurtubise <316k@legtux.org>
2014-02-19 00:59:46 -0500

    Basic homepage and fortune displayer

---
+++ fortune.php
@@ -0,0 +1,46 @@
+<?php
+
+if(isset($_GET['all'])) {
+    $files = scandir('fortunes');
+    // Removes '.'
+    array_shift($files);
+    // Removes '..'
+    array_shift($files);
+    
+    $file_name = 'fortunes/' . $files[mt_rand(0, count($files))];
+} else if(isset($_GET['url'])) {
+    $file_name = preg_match('#^https?://.*#i', $_GET['url']) ? $_GET['url'] : 'http://' . $_GET['url'];
+} else if(isset($_GET['file']) && file_exists('fortunes/' . $_GET['file'])) {
+    $file_name = 'fortunes/' . $_GET['file'];
+} else {
+    header("HTTP/1.0 404 Not Found");
+    header("Location: ./");
+    exit();
+}
+
+// Gets all fortune lines as an array
+if(!($file = @file($file_name))) {
+    die('File error');
+}
+
+$fortunes = array();
+$offset = 0;
+
+foreach($file as $index => $line) {
+    
+    // Offset must exist
+    if(count($fortunes) <= $offset) {
+        $fortunes[$offset] = "";
+    }
+    
+    if($line == "%\n") {
+        $offset++;
+    } else {
+        $fortunes[$offset] .= $line;
+    }
+}
+
+
+$fortune = $fortunes[mt_rand(0, count($fortunes))];
+
+echo isset($_GET['html']) ? nl2br($fortune) : $fortune;
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <header>
+            <h1>Welcome to web-fortune !</h1>
+            <small>~ Helping people loose their time since 1905 ~ </small>
+        </header>
+        <h2>Why should you use web-fortune.org ?</h2>
+        <p>This service aims to be an emergency fallback for when the <code>fortune</code> UNIX program is unreachable and you are bored. For example :</p>
+        <ul>
+            <li>You are at work and your code is compiling (or not)</li>
+            <li>You are using a friend's laptop running under Windope and you have too much time</li>
+            <li>You lost your root password and you cannot install anything anymore</li>
+        </ul>
+        <p>web-fortune.org can also be used to entertain visitors on your website :<p>
+        <code>
+            &lt;?php
+            echo file_get_contents('http://web-fortune.org/fortune.php?computers');
+            ?&gt;
+        </code>
+        <h2>How to use web-fortune.org ?</h2>
+        <p>It's pretty simple.
+        <br />
+        Just go click on one of the many centers of interest below (e.g. <a href="http://web-fortune.org/fortune.php?html&amp;computers">computers</a>)</p><p>For access from a terminal : </p>
+        <code>
+            curl http://web-fortune.org/fortune.php?computers
+        </code>
+        <h2>Available fortune files</h2>
+        <ul>
+            <li><a href="fortune.php?html&amp;all"><b>All</b></a> <small><a href="fortune.php?all">[raw text]</a></small></li>
+            <?php 
+            $fortune_files = array();
+            foreach(new DirectoryIterator('fortunes') as $file) {
+                if(!$file->isDot()) {
+                    $fortune_files[] = (string) $file;
+                    echo '';
+                }
+            }
+            
+            sort($fortune_files);
+            
+            foreach($fortune_files as $file): ?>
+                <li><a href="fortune.php?html&amp;file=<?php echo $file ?>"><?php echo $file ?></a> <small><a href="fortune.php?file=<?php echo $file ?>">[raw text]</a></small></li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
