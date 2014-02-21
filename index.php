422f0af71140b2b98be1b97f5a392f1938d5ce98
Nicolas Hurtubise <316k@legtux.org>
2014-02-20 23:49:52 -0500

    Corrects urls and html code in home page

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>Online fortune generator</title>
+        <title>Online fortune generator</title>
+        <meta charset="utf-8" />
     </head>
     <body>
         <header>
-            <h1>Welcome to web-fortune !</h1>
-            <small>~ Helping people loose their time since 1905 ~ </small>
+            <h1>Welcome to Web-fortune, the online fortune generator !</h1>
         </header>
-        <h2>Why should you use web-fortune.org ?</h2>
+        <h2>Why should you use this service ?</h2>
         <p>This service aims to be an emergency fallback for when the <code>fortune</code> UNIX program is unreachable and you are bored. For example :</p>
         <ul>
             <li>You are at work and your code is compiling (or not)</li>
             <li>You are using a friend's laptop running under Windope and you have too much time</li>
             <li>You lost your root password and you cannot install anything anymore</li>
         </ul>
-        <p>web-fortune.org can also be used to entertain visitors on your website :<p>
+        <p>Web-fortune can also be used to entertain visitors on your website :<p>
         <code>
             &lt;?php
-            echo file_get_contents('http://web-fortune.org/fortune.php?computers');
+            echo file_get_contents('http://wasting-ti.me/fortune/fortune.php?file=computers');
             ?&gt;
         </code>
-        <h2>How to use web-fortune.org ?</h2>
+        <h2>How to use Web-fortune ?</h2>
         <p>It's pretty simple.
         <br />
-        Just go click on one of the many centers of interest below (e.g. <a href="http://web-fortune.org/fortune.php?html&amp;computers">computers</a>)</p><p>For access from a terminal : </p>
+        Just go click on one of the many centers of interest below (e.g. <a href="http://wasting-ti.me/fortune/fortune.php?html&amp;computers">computers</a>)</p><p>For access from a terminal : </p>
         <code>
-            curl http://web-fortune.org/fortune.php?computers
+            curl http://wasting-ti.me/fortune/fortune.php?computers
         </code>
         <h2>Available fortune files</h2>
         <ul>
@@ -49,3 +49,4 @@
         <h2>Source code and license</h2>
         <p>This program is licensed under the <a href="LICENSE">Apache v2 License</a>. See https://github.com/316k/web-fortune for source code.</p>
     </body>
+</html>
